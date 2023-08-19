d91c5897a272bb1b1c48b3badb52331b89408569
cristianmeelo <cristianmeelo@gmail.com>
2023-08-19 09:40:16 -0300

    :sparkler: feat: styled title

--- next-alura-cases/src/app/components/Link/index.tsx
+++ next-alura-cases/src/app/components/Link/index.tsx
@@ -1,21 +1,10 @@
-import React, { ReactNode } from 'react'
-import NextLink  from 'next/link'
+import React, { ReactNode } from 'react';
+import NextLink from 'next/link';
 
-type LinkProps = {
-    children: ReactNode;
-    href: string;
-  };
-
-
-export const Link = ({children, href, ...props}: LinkProps) => {
+export const Link = ({ children, href, ...props }: LinkProps) => {
   return (
-<NextLink href={href} passHref>
-<span style={{ color: 'red' }}>
-{children}
-
-</span>
+    <NextLink href={href} passHref>
+      <span style={{ color: 'red' }}>{children}</span>
     </NextLink>
-  )
-}
-
-
+  );
+};
